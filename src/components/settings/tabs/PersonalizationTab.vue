bb81379e4b2dcd1f17fdf3954e127b91e1855479
teidesu <me@tei.su>
2020-07-29 00:10:10 +0300

    feat(settings): change language
    also added a few separators and fixed layout in PersonalizationTab

--- src/components/settings/tabs/PersonalizationTab.vue
+++ src/components/settings/tabs/PersonalizationTab.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        <v-row>
+        <v-select
+            v-model="language"
+            :label="$t('Pages.Settings.UiLanguage')"
+            :items="uiLanguages"
+        >
+            <template #item="{ item }">
+                <v-list-item-title>
+                    <span :class="'mr-2 flag-' + item.value" />
+                    {{ item.text }}
+                </v-list-item-title>
+            </template>
+        </v-select>
+
+        <v-divider class="mb-2" />
+
+        <v-row no-gutters>
             <v-col
                 cols="12"
                 sm="6"
@@ -20,6 +35,9 @@
                 />
             </v-col>
         </v-row>
+
+        <v-divider class="mb-2" />
+
         <v-select
             v-model="preferredNameLanguage"
             :items="nameLanguageItems"
@@ -30,6 +48,8 @@
             class="caption text--secondary"
             v-html="$t('Pages.Settings.PreferredNameLanguageShiki')"
         />
+
+        <v-divider class="mb-2" />
 
         <v-switch
             v-model="expandAllViewer"
@@ -74,11 +94,17 @@ import VColorField from '@/components/common/fields/VColorField.vue'
 import { authStore, configStore } from '@/store'
 import { Media } from '@/types/media'
 import ReorderList from '@/components/common/fields/ReorderList.vue'
+import { changeLanguage } from '@/plugins/vue-i18n'
+import { setUserLanguage } from '@/api/user'
+import { iziToastError } from '@/plugins/izitoast'
+import { languages } from '@/utils/i18n'
 
 @Component({
     components: { ReorderList, VColorField }
 })
 export default class PersonalizationTab extends Vue {
+    uiLanguages = languages
+
     get moderator (): boolean {
         return authStore.user?.moderator ?? false
     }
@@ -89,6 +115,18 @@ export default class PersonalizationTab extends Vue {
 
     get dark (): boolean {
         return configStore.dark
+    }
+
+    get language (): string {
+        return configStore.language
+    }
+
+    set language (val: string) {
+        configStore.merge({
+            language: val
+        })
+        changeLanguage(val)
+        setUserLanguage(val).catch(iziToastError)
     }
 
     get primaryColor (): string {
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,5 +1,22 @@
 export let defaultLanguage = 'en'
+
 const browserLanguage = navigator.language.split('-')[0]
 if (browserLanguage === 'ru') {
     defaultLanguage = 'ru'
 }
+
+
+export const languages = Object.freeze([
+    {
+        text: 'English',
+        value: 'en'
+    },
+    {
+        text: 'Русский',
+        value: 'ru'
+    }
+].sort((a, b) => {
+    return a.value === defaultLanguage
+        ? -1 : b.value === defaultLanguage
+            ? 1 : a.value > b.value ? 1 : -1
+}))

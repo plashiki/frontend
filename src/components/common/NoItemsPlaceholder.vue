9eaa853b0bf624145cc57b2fb59a25ef1f2b2172
teidesu <me@tei.su>
2020-07-29 16:39:12 +0300

    fix(no-items-placeholder): alignment when text overflows 1 line

--- src/components/common/NoItemsPlaceholder.vue
+++ src/components/common/NoItemsPlaceholder.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="row align-center justify-center grey--text body-1 font-weight-bold"
+        class="row align-center justify-center grey--text body-1 font-weight-bold text-center"
         style="height: 170px"
         v-bind="$attrs"
         v-html="text"

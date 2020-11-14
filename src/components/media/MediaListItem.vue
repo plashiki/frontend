27b4ac92da984ff4fa98fdac801c90970b4f4919
teidesu <me@tei.su>
2020-11-14 20:45:33 +0300

    fix(media-list-item): genre name x2

--- src/components/media/MediaListItem.vue
+++ src/components/media/MediaListItem.vue
@@ -95,7 +95,7 @@ export default class MediaListItem extends Vue {
         }
 
         if (this.item.genres?.length) {
-            ret.push(this.$t('Genres.' + this.item.genres[0].name))
+            ret.push(this.$t(this.item.genres[0].name))
         }
 
         if (this.item.year) {

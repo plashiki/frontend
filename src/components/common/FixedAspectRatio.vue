8f1de60f76564d8a6ad6a445085c496c4fa9df85
teidesu <me@tei.su>
2020-11-26 22:19:58 +0300

    feat(FixedAspectRatio): maintain own aspect ratio, and not aspect ratio of wrapped element

--- src/components/common/FixedAspectRatio.vue
+++ src/components/common/FixedAspectRatio.vue
@@ -20,29 +20,38 @@ import { Component, Vue, Ref, Prop } from 'vue-property-decorator'
 @Component({})
 export default class FixedAspectRatio extends Vue {
     @Prop({ type: Number, required: true }) aspectRatio!: number
+    @Prop({ type: Boolean, default: false }) forContainer!: boolean
 
     @Ref() container!: HTMLDivElement
     @Ref() wrap!: HTMLDivElement
 
     onResize (): void {
         const width = this.container.offsetWidth
-        const height = this.container.offsetHeight
-        const elementRatio = width / height
-        let realWidth = width
-        let realHeight = height
-        if (elementRatio > this.aspectRatio) {
-            realWidth = Math.floor(height * this.aspectRatio)
+        if (this.forContainer) {
+            this.container.style.height = width / this.aspectRatio + 'px'
+            this.wrap.style.width = '100%'
+            this.wrap.style.height = '100%'
+            this.wrap.style.left = '0'
+            this.wrap.style.top = '0'
         } else {
-            realHeight = Math.floor(width / this.aspectRatio)
+            const height = this.container.offsetHeight
+            const elementRatio = width / height
+            let realWidth = width
+            let realHeight = height
+            if (elementRatio > this.aspectRatio) {
+                realWidth = Math.floor(height * this.aspectRatio)
+            } else {
+                realHeight = Math.floor(width / this.aspectRatio)
+            }
+
+            const x = (width - realWidth) / 2
+            const y = (height - realHeight) / 2
+
+            this.wrap.style.width = realWidth + 'px'
+            this.wrap.style.height = realHeight + 'px'
+            this.wrap.style.left = x + 'px'
+            this.wrap.style.top = y + 'px'
         }
-
-        const x = (width - realWidth) / 2
-        const y = (height - realHeight) / 2
-
-        this.wrap.style.width = realWidth + 'px'
-        this.wrap.style.height = realHeight + 'px'
-        this.wrap.style.left = x + 'px'
-        this.wrap.style.top = y + 'px'
     }
 
     addListeners (): void {
